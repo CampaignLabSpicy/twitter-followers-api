d25f550ddb42f9fc477c8a6c4228784e598a5bb4
monkeybrie <morkeltry@mailfence.com>
2019-09-28 21:30:40 +0100

    added import for matcher

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ const cors = require('cors')
 const Twitter = require('twitter')
 require('dotenv').config()
 
+const matcher = require('./matcher')
+
 const app = express()
 // Check cors permissions before production
 app.use(cors({
