889457aca9540551a6a8d6b1c09b381b04af4556
Shinies <github@morkeltry.33mail.com>
2019-10-26 15:40:16 +0100

    basic lookups for postcodes.io, with shims

--- server/location/externals.js
+++ server/location/externals.js
@@ -1,14 +1,35 @@
 const postcodes = require('node-postcodes.io');
 
+const nodePostcodesIoShim = response => response.result;
 
-const lookupPc = (pc, options = {}) => {
+const lookupPc = async (pc, options = {}) => {
   const result = await postcodes.lookup(pc, options)
+
+  return result
+}
+
+const constituencyFromPcioLookup = result => {
+  if (result === null || !result.codes || !result.codes.parliamentary_constituency)
+    throw new Error ('some kinda error');
+
+  const pcObj = {}
+  pcObj [result.codes.parliamentary_constituency] = result.parliamentary_constituency ;
+  return pcObj
 }
 
 const testPcs = [
   'N4 3DL'
 ]
+//
+// testPcs.forEach (pc => {
+//   console.log( lookupPc());
+// });
 
-testPcs.forEach (pc => {
-  console.log( lookupPc());
-});
+let timer = Date.now()
+lookupPc ('N4 3DL')
+  .then (nodePostcodesIoShim)
+  .then (constituencyFromPcioLookup)
+  .then (result=> {
+    console.log(`${Date.now()-timer}ms:`);
+    console.log(result);
+  });
